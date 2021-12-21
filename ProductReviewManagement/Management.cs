214663c2773ec18e937668d952508875c2b2947a
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-21 14:23:28 +0530

    [gunasekhar9] add : Product Review by DataTable is Done

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -46,5 +46,24 @@ namespace ProductReviewManagement
             var records = (from Product in productList select Product).Skip(5).ToList();
             Display(records);
         }
+        DataTable dataTable = new DataTable();
+        public void ProductReviewDatatable(List<ProductReview> productList)
+        {
+            dataTable.Columns.Add("ProductID").DataType = typeof(Int32);
+            dataTable.Columns.Add("UserID").DataType = typeof(Int32);
+            dataTable.Columns.Add("Rating").DataType = typeof(Int32);
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("ISLike").DataType = typeof(bool);
+            foreach(var data in productList)
+            {
+                dataTable.Rows.Add(data.ProductID, data.UserID, data.Rating, data.Review, data.ISLike);
+            }
+            var productTable = from Product in dataTable.AsEnumerable() select Product;
+            foreach (DataRow Product in productTable)
+            {
+                Console.WriteLine("ProductID : " + Product.Field<int>("ProductID") + "\t" + "UserID : " + Product.Field<int>("UserID") + "\t" + "Rating : " + Product.Field<int>("Rating") + "\t" + "Review : " + Product.Field<string>("Review") + "\t" + "ISLike : " + Product.Field<bool>("ISLike"));
+            }
+        }
+
     }
 }
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -24,7 +24,7 @@ namespace ProductReviewManagement
             Management management = new Management();
             while (flag)
             {
-                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Retrieve Records using Product ID which is Rating > 3 \n 4. The Number of Recirds of ProductID \n 5. Retrieve Product ID and Review \n 6. Skip Top Five Records \n 7. Exit");
+                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Retrieve Records using Product ID which is Rating > 3 \n 4. The Number of Recirds of ProductID \n 5. Retrieve Product ID and Review \n 6. Skip Top Five Records \n 7. Product Review of Datatable \n 8. Exit");
                 int number = Convert.ToInt32(Console.ReadLine());
                 switch (number)
                 {
@@ -48,6 +48,9 @@ namespace ProductReviewManagement
                         Console.WriteLine("Top Five records are Skipped, And Above records are Remaining Records");
                         break;
                     case 7:
+                        management.ProductReviewDatatable(productList);
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
